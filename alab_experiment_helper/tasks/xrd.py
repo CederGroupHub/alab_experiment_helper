9ce0d51aac39073ef65062bc56c281b895c408c4
rekumar <rekumar@lbl.gov>
2022-08-08 14:05:18 -0700

    working to xrd

--- alab_experiment_helper/tasks/xrd.py
+++ alab_experiment_helper/tasks/xrd.py
@@ -4,15 +4,16 @@ from alab_experiment_helper.sample import Sample
 from alab_experiment_helper.tasks.base import task
 
 
-@task("XRD")
+@task("Diffraction")
 def xrd(sample: Sample, *, schema: Literal["fast_10min", "slow_30min"] = "fast_10min"):
     """
     Do xrd on the given sample with the given ``schema``. The schema is either ``fast_10min`` or ``slow_30min``.
 
     Args:
         sample: Sample to do xrd on.
-        schema: Schema to use. By default, a fast 10min schema is used.
+        schema: Schema to use. By default, a fast 10min schema is used. Options = ["fast_10min", "slow_30min"]
     """
     return {
+        "sample": sample.name,
         "schema": schema,
     }
